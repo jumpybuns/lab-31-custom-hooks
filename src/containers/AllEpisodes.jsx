debdde92a1e60edf5309340f18276da9a132214b
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-09 16:17:20 -0800

    episodes hook worked!

--- src/containers/AllEpisodes.jsx
+++ src/containers/AllEpisodes.jsx
@@ -1,23 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { fetchEpisodes } from '../services/rickAndMortyAPI';
 import EpisodeList from '../components/episodes/EpisodeList';
+import { useEpisodes } from '../hooks/Episodes';
 
-function AllEpisodes() {
-  const [episodes, setEpisodes] = useState([]);
-  const [loading, setLoading] = useState(true);
+const AllEpisodes = () => {
+  const { episodes } = useEpisodes();
 
-  useEffect(() => {
-    fetchEpisodes().then((res) => {
-      setEpisodes(res), setLoading(false);
-    });
-  }, []);
-
-  if (loading) return <h1>Loading...</h1>;
-  return (
-    <>
-      <EpisodeList episodes={episodes} />
-    </>
-  );
-}
-
+  return <EpisodeList episodes={episodes} />;
+};
 export default AllEpisodes;
