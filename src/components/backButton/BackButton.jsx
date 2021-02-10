1600064cdd02bca5bc2a465d4b7d1bcd1e6bc062
Jumpybuns <ethanpiercepresents@gmail.com>
2021-02-09 21:21:36 -0800

    add home link

--- src/components/backButton/BackButton.jsx
+++ src/components/backButton/BackButton.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const BackButton = () => {
-  <Link to="/">
-    <p>Back Home</p>
-  </Link>;
-};
-
-export default BackButton;
+export default class BackButton extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Switch>
+            <Route to="/">Home</Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
+}
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 // import BackButton from '../backButton/BackButton';
 
 export default class Header extends Component {
